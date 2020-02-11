41abb12aa7e408be43747beff4eae82524c1abb2
Bryan Amezcua <34616679+BryanAmezcua@users.noreply.github.com>
2020-02-10 22:36:25 -0800

    Update
    Commented out the "TrackList" component to allow fake song data to be passed across components

--- src/Components/PlayList/PlayList.js
+++ src/Components/PlayList/PlayList.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import TrackList from '../TrackList/TrackList.js'
 import './PlayList.css';
 
 class PlayList extends Component {
     render() {
         return (
             <div className="Playlist">
-                <input value="New Playlist"/>
-                {/*<!-- Add a TrackList component -->*/}
+                <input defaultValue='New Playlist' />
+                {/*<TrackList/>*/}
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         );
