75a6c95313414ce484a953dc4f9ab0662dfda032
Bryan Amezcua <34616679+BryanAmezcua@users.noreply.github.com>
2020-02-10 22:37:54 -0800

    Update
    Added a prop to the "TrackList" component that allows data to be passed from state within "App.js"

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './SearchResults.css';
+import TrackList from '../TrackList/TrackList.js';
 
 class SearchResults extends Component {
     render() {
         return (
             <div className="SearchResults">
                 <h2>Results</h2>
-                {/*<!-- Add a TrackList component -->*/}
+                <TrackList tracks={this.props.searchResults} />
             </div>
         );
     }
