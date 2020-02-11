7818f8f3c40d87f4af7445c7a8dda2a9b761ff83
Bryan Amezcua <34616679+BryanAmezcua@users.noreply.github.com>
2020-02-10 22:34:10 -0800

    Added state with test data
    Added state to this component with an array of objects as test "song" data.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,6 +7,27 @@ import SearchResults from '../SearchResults/SearchResults.js';
 import PlayList from '../PlayList/PlayList.js';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: 'Song Name',
+          artist: 'Drake',
+          album: "Drake's Album",
+          id: 1
+        },
+        {
+          name: 'Song Name 2',
+          artist: 'Future',
+          album: 'Future Album',
+          id: 2
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <div>
@@ -14,8 +35,8 @@ class App extends Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
-            <PlayList />
+            <SearchResults searchResults={this.state.searchResults}/>
+            <PlayList playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
       </div>
